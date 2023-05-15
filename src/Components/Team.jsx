004f72e8cef3c447914671a33d6a81d1cd2318e2
Pranav <95164530+thefakepranav@users.noreply.github.com>
2023-05-15 16:14:56 +0530

    added team member mobile view

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import TeamMemberCard from "./TeamMemberCard";
 import ClubMemberCard from "./ClubMemberCard";
 import devansh from "./devansh.jpg";
+import TeamMemberMobile from "./TeamMemberMobile";
 
 const Team = () => {
   return (
-    <div className="mx-20">
+    <div className="mx-6 lg:mx-20">
       <h1 className="lg:text-[64px] text-[32px]">The People Behind</h1>
-      {/* <TeamMemberCard /> */}
-      <div className="flex flex-row flex-wrap w-full justify-center [&>div]:mt-6">
+
+      <div>
+        <TeamMemberMobile
+          name="Varun Nagpal"
+          por="Secy"
+          oneLiner="I'll get people to think of one liners and will try to think of 3d stuff"
+          githubUrl="#"
+          img={devansh}
+        />
+      </div>
+
+      <div className="flex flex-row flex-wrap w-full justify-center [&>div]:mt-6 hidden lg:block">
         <ClubMemberCard
           name="Varun Nagpal"
           por="Secy"
