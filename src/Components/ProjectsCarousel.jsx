3b8551c46f44262160342197351e78149d41a4dc
Pranav <95164530+thefakepranav@users.noreply.github.com>
2023-05-15 22:21:48 +0530

    landing page projects carousel responsive

--- src/Components/ProjectsCarousel.jsx
+++ src/Components/ProjectsCarousel.jsx
@@ -10,62 +10,114 @@ import "./ProjectCarousel.css";
 
 const ProjectsCarousel = () => {
   return (
-    <div>
-      <Splide
-        hasTrack={false}
-        options={{
-          rewind: true,
-          autoWidth: true,
-          autoplay: true,
-          interval: 2000,
-          lazyLoad: "sequential",
-          keyboard: "global",
-          perPage: 2,
-          perMove: 1,
-          pagination: false,
-          // focus: "center center",
-          type: "loop",
-          easing: "ease",
-          arrows: true,
-          // focus: 1,
-          gap: 20,
-          padding: 100,
-        }}
-      >
-        <SplideTrack className="mx-20">
-          <SplideSlide
-            id="carouselCard1"
-            onMouseOver={(e) => {
-              console.log(e.target);
-            }}
-          >
-            <div>
+    <>
+      {/* desktop view */}
+      <div className="max-lg:hidden">
+        <Splide
+          hasTrack={false}
+          options={{
+            rewind: true,
+            autoWidth: true,
+            autoplay: true,
+            interval: 2000,
+            lazyLoad: "sequential",
+            keyboard: "global",
+            perPage: 2,
+            perMove: 1,
+            pagination: false,
+            // focus: "center center",
+            type: "loop",
+            easing: "ease",
+            arrows: true,
+            // focus: 1,
+            gap: 20,
+            padding: 100,
+          }}
+        >
+          <SplideTrack className="mx-20">
+            <SplideSlide
+              id="carouselCard1"
+              onMouseOver={(e) => {
+                console.log(e.target);
+              }}
+            >
+              <div>
+                <img src={TempCard} />
+              </div>
+            </SplideSlide>
+            <SplideSlide>
               <img src={TempCard} />
-            </div>
-          </SplideSlide>
-          <SplideSlide>
-            <img src={TempCard} />
-          </SplideSlide>
-          <SplideSlide>
-            <img src={TempCard} />
-          </SplideSlide>
-          <SplideSlide>
-            <img src={TempCard} />
-          </SplideSlide>
-        </SplideTrack>
+            </SplideSlide>
+            <SplideSlide>
+              <img src={TempCard} />
+            </SplideSlide>
+            <SplideSlide>
+              <img src={TempCard} />
+            </SplideSlide>
+          </SplideTrack>
 
-        <div className="splide__arrows">
-          <LeftArrowIcon
-            style={{ backgroundColor: "black" }}
-            className="splide__arrow splide__arrow--prev "
-          />
-          <RightArrowIcon
-            style={{ backgroundColor: "black" }}
-            className="splide__arrow splide__arrow--next"
-          />
-        </div>
-      </Splide>
-    </div>
+          <div className="splide__arrows">
+            <LeftArrowIcon
+              style={{ backgroundColor: "black" }}
+              className="splide__arrow splide__arrow--prev "
+            />
+            <RightArrowIcon
+              style={{ backgroundColor: "black" }}
+              className="splide__arrow splide__arrow--next"
+            />
+          </div>
+        </Splide>
+      </div>
+
+      {/* mobile view */}
+      <div className="lg:hidden">
+        <Splide
+          hasTrack={false}
+          options={{
+            rewind: true,
+            autoWidth: true,
+            autoplay: true,
+            interval: 2000,
+            lazyLoad: "sequential",
+            keyboard: "global",
+            perPage: 2,
+            perMove: 1,
+            pagination: false,
+            // focus: "center center",
+            type: "loop",
+            easing: "ease",
+            arrows: true,
+            // focus: 1,
+            // gap: 20,
+            padding: 24,
+          }}
+        >
+          <SplideTrack className="">
+            <SplideSlide
+              id="carouselCard1"
+              onMouseOver={(e) => {
+                console.log(e.target);
+              }}
+            >
+              <div>
+                <img src={TempCard} />
+              </div>
+            </SplideSlide>
+            <SplideSlide>
+              <img src={TempCard} />
+            </SplideSlide>
+            <SplideSlide>
+              <img src={TempCard} />
+            </SplideSlide>
+            <SplideSlide>
+              <img src={TempCard} />
+            </SplideSlide>
+          </SplideTrack>
+
+          
+        </Splide>
+      </div>
+    </>
   );
 };
 
