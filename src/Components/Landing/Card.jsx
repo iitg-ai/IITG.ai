450193d047c6e0b3d8f3ccd193e83fffae4af2fb
Pranav <95164530+thefakepranav@users.noreply.github.com>
2023-05-17 18:46:53 +0530

    mlai mlrw people images

--- src/Components/Landing/Card.jsx
+++ src/Components/Landing/Card.jsx
@@ -7,20 +7,19 @@ const Card = (props) => {
         style={{
           fontSize: 12,
           fontFamily: "Nohemi",
-          opacity: 0.5,
           display: "inline",
           boxSizing: "border-box",
-          padding: "10px 12px 6px 0px",
+          padding: "10px 12px 6px 12px",
           borderRadius: "100px",
         }}
         className={
-          props.flag === 1 ? `text-white backdrop-blur-sm	bg-[#282828] pl-12` : `pl-0`
+          props.flag === 1 ? `text-[#FFFFFF] background-blur-sm bg-[#282828] pl-12` : `pl-0`
         }
       >
         {props.anchor}
       </h6>
       <h1 className="text-[24px] lg:text-[44px] ">{props.heading}</h1>
-      <p style={{ fontSize: 14, opacity: 0.7 }} className="lg:w-3/5">
+      <p style={{ fontSize: 14, opacity: 0.7 }} className="lg:w-4/5">
         {props.description}
       </p>
     </div>
--- src/Components/Landing/Events.jsx
+++ src/Components/Landing/Events.jsx
@@ -8,16 +8,17 @@ const Events = () => {
     <>
       {/* desktop view */}
       <div className="mx-20 mt-28 flex flex-row justify-between max-lg:hidden">
-        <div className="w-1/2 self-end">
+        <div className="w-1/2">
           <Link to="/coursePage">
-            <img src={temp1} />
+            <img src="images/mlrw.png" />
           </Link>
           <Card
-            className="ml-8 -mt-80"
+            // className="ml-8 -mt-72 mr-8"
+            className="absolute w-2/5 ml-8 -mt-72 mr-8"
             flag={1}
-            anchor="Course"
-            heading=""
-            description="The course offers a comprehensive blend of high-quality resources including engaging videos, informative blogs, weekly assignments, and exciting hackathons to assess your knowledge and skills. The course is one of a kind, free, open for all and has no prerequisites."
+            anchor="KEYNOTES"
+            heading="MLRW"
+            description="MLRW (Machine Learning Research Week) is an innovative platform designed to provide students with a unique opportunity to engage with and learn from high-achieving individuals in the field of Artificial Intelligence (AI) and Machine Learning (ML). This week-long event brings together renowned researchers, industry professionals, and educators who have made significant contributions to the field, offering students an invaluable chance to broaden their knowledge and expand their network within the AI community."
           />
         </div>
 
@@ -30,14 +31,15 @@ const Events = () => {
           />
           <div>
             <Link to="mlrw">
-              <img src={temp2} />
+              <img src="images/mlai.png" />
             </Link>
             <Card
-              className="ml-24 -mt-44"
+              // className="ml-20 -mt-56 "
+            className="absolute ml-20 -mt-[200px]"
               flag={1}
-              anchor="Keynotes"
-              heading="MLRW"
-              description="MLRW (Machine Learning Research Week) is an innovative platform designed to provide students with a unique opportunity to engage with and learn from high-achieving individuals in the field of Artificial Intelligence (AI) and Machine Learning (ML). This week-long event brings together renowned researchers, industry professionals, and educators who have made significant contributions to the field, offering students an invaluable chance to broaden their knowledge and expand their network within the AI community."
+              anchor="COURSE"
+              heading="ML.AI"
+              description="The course offers a comprehensive blend of high-quality resources including engaging videos, informative blogs, weekly assignments, and exciting hackathons to assess your knowledge and skills. The course is one of a kind, free, open for all and has no prerequisites."
             />
           </div>
         </div>
