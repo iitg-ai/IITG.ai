e27c1e9ddda26b2cd6355beecd36442f10444cc9
Pranav <95164530+thefakepranav@users.noreply.github.com>
2023-07-14 22:00:26 +0530

    week6 table updated.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Analytics } from '@vercel/analytics/react';
 import "./Components/HoverMessage.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CoursePage from "./Pages/CoursePage";
@@ -6,25 +7,30 @@ import LandingPage from "./Pages/LandingPage";
 import Footer from "./ConstantComponents/Footer";
 import LeaderboardPage from "./Pages/LeaderboardPage";
 
-
 function App() {
   return (
-    <div className={`App bg-black bg-no-repeat`}>
-      <div id="hover" className="hoverMessage"></div>
+    <>
+      <div className={`App bg-black bg-no-repeat`}>
+        <div id="hover" className="hoverMessage"></div>
 
-      {/* <TeamPage /> */}
-      <Router>
-        <div className="pb-60">
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            {/* <Route path="/team" element={<TeamPage />} /> */}
-            <Route path="/coursePage" element={<CoursePage />} />
-            <Route path="/coursePage/leaderboard" element={<LeaderboardPage />} />
-          </Routes>
-        </div>
-        <Footer />
-      </Router>
-    </div>
+        {/* <TeamPage /> */}
+        <Router>
+          <div className="pb-60">
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              {/* <Route path="/team" element={<TeamPage />} /> */}
+              <Route path="/coursePage" element={<CoursePage />} />
+              <Route
+                path="/coursePage/leaderboard"
+                element={<LeaderboardPage />}
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </Router>
+      </div>
+      <Analytics mode={'production'} />;
+    </>
   );
 }
 
